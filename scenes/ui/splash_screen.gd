993108d01e1b21b529d431b2a15d2fcdc6f91fdb
dalifyyy <d4lified@gmail.com>
2026-01-19 08:30:36 +0800

    ....

---
+++ scenes/ui/splash_screen.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("show")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/ui/title_screen.tscn")
